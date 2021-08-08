1a3b24800ec5bdc07da002541f78daa7beeb0c61
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-08-08 18:01:31 -0400

    Added logic to the canvas component to change paint color and size

--- Front-End/app/src/app/components/canvas/canvas.component.ts
+++ Front-End/app/src/app/components/canvas/canvas.component.ts
@@ -17,6 +17,8 @@ export class CanvasComponent implements OnInit {
   context: CanvasRenderingContext2D;
   width: number;
   height: number;
+  lineColor: string;
+  lineWidth: number;
 
   constructor(private socket: Socket)  { }
     
@@ -27,6 +29,9 @@ export class CanvasComponent implements OnInit {
       pos: {x: 0, y: 0},
       pos_prev: false
     };
+    this.lineColor = 'black';
+    this.lineWidth = 4;
+
     this.canvas = <HTMLCanvasElement> document.getElementById('drawing');
     this.context =  <CanvasRenderingContext2D>this.canvas.getContext('2d');
     
@@ -34,6 +39,8 @@ export class CanvasComponent implements OnInit {
     this.height = window.innerHeight;
     this.canvas.width = this.canvas.getBoundingClientRect().width;
     this.canvas.height = this.canvas.getBoundingClientRect().height;
+    this.context.lineWidth = this.lineWidth;
+    this.context.strokeStyle = this.lineColor;
     
     this.canvas.addEventListener('mousedown', () => {
       this.mouse.click = true;
@@ -43,7 +50,7 @@ export class CanvasComponent implements OnInit {
     });
     this.canvas.addEventListener('mousemove', (uMouse: any)=> {    
       this.mouse.pos.x = uMouse.clientX   / this.canvas.width; 
-      this.mouse.pos.y = uMouse.clientY / (this.canvas.height); 
+      this.mouse.pos.y = uMouse.clientY / this.canvas.height; 
       this.mouse.move = true;
     })
     this.socket.fromEvent('draw_line').subscribe((data: any) => {
@@ -58,16 +65,36 @@ export class CanvasComponent implements OnInit {
 
   draw(data: any) {
     var line = data.line;
+    this.context.strokeStyle = data.line[2];
+    this.context.lineWidth = data.line[3];
     this.context.beginPath();
     this.context.moveTo(line[0].x * this.canvas.width - this.canvas.offsetLeft, line[0].y * this.canvas.height - this.canvas.offsetTop);
     this.context.lineTo(line[1].x * this.canvas.width - this.canvas.offsetLeft, line[1].y * this.canvas.height - this.canvas.offsetTop);
     this.context.stroke();
   }
 
+  changeColor(color: string) {
+    this.lineColor = color;
+  }
+  changeWidth(size: string) {
+    if (size == 'small')
+      this.lineWidth = 2.5;
+    
+    else if (size == 'medium')
+      this.lineWidth = 4.0;
+    
+    else if (size == 'large')
+      this.lineWidth = 6.0;
+
+    console.log
+    
+  }
+
   checkDraw = () => {
     if (this.mouse.click && this.mouse.move && this.mouse.pos_prev)
     {
-      this.socket.emit('draw_line', { line: [this.mouse.pos, this.mouse.pos_prev] });
+      this.socket.emit('draw_line', 
+      { line: [this.mouse.pos, this.mouse.pos_prev, this.lineColor, this.lineWidth] });
       this.mouse.move = false;
     }
     this.mouse.pos_prev = {x: this.mouse.pos.x, y: this.mouse.pos.y};
