ffc92bd769bc3c8cb3bc5386dc308346110b860a
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-08-11 16:25:14 -0400

    Created component to display the hidden word

---
+++ Front-End/app/src/app/components/word/word.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+@Component({
+  selector: 'app-word',
+  templateUrl: './word.component.html',
+  styleUrls: ['./word.component.css']
+})
+export class WordComponent implements OnInit {
+
+  word: string;
+  hiddenWord: string; 
+
+  constructor(private socket: Socket) { }
+
+  ngOnInit(): void {
+    this.socket.fromEvent('getWord').subscribe((data: any) => {
+      this.word = data;
+      this.hiddenWord = this.hideWord(this.word);
+    });
+  }
+
+  hideWord(word: string) {
+    let temp = word;
+    var re = /[a-zA-z]/gi;
+    var newstr = temp.replace(re, "_");
+    console.log(newstr);
+    return newstr;
+  }
+}
