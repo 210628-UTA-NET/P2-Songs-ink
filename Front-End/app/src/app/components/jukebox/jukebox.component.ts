16bb0fa507e08c4f0469e22299bffc1d2dfd4248
Caleb-Huss <caleb.huss97@gmail.com>
2021-08-12 16:33:59 -0500

    jukebox in progress

--- Front-End/app/src/app/components/jukebox/jukebox.component.ts
+++ Front-End/app/src/app/components/jukebox/jukebox.component.ts
@@ -2,29 +2,53 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MusicService } from '../../services/music.service';
 import { Song } from '../../models/Song';
 import { AuthService } from '@auth0/auth0-angular';
-import { ProfileService } from 'src/app/services/profile.service';
+import { ProfileService } from '../../services/profile.service';
+import { Profile } from '../../models/Profile';
+import { User } from '@auth0/auth0-spa-js';
 
 @Component({
   selector: 'app-jukebox',
   templateUrl: './jukebox.component.html',
   styleUrls: ['./jukebox.component.css']
 })
-export class JukeboxComponent implements OnInit,AfterViewInit {
-
+export class JukeboxComponent implements OnInit {
+  audioVolume:number = 0.05;
   Songs: Song[] = [];
   songUrl: string;
   song1: string ="song1";
-  song2: string;
-  song3: string;
+  song2: string ="song2";
+  song3: string ="song3";
   songIndex: number =1;
   changeSongsCost: number = -10;
-  nextSongCost: number = -25;
+  skipSongCost: number = -25;
   currentSongTime: number =0;
   totalSongTime: number = 123;
-  constructor(private musicApi: MusicService, public auth: AuthService) { }
+  currentPlayer: Profile= {
+    currentScore: 0,
+    email: "",
+    playerScore: 0,
+    playerName: "",
+    gamesPlayed: 0,
+    id: 0,
+    customWords: []
+  };
+  constructor(private profApi: ProfileService, private musicApi: MusicService, public auth: AuthService) { }
 
   ngOnInit(): void {
     this.getSongs();
+    if(this.auth.isAuthenticated$)
+    {
+      console.log("isloggedin")
+      this.auth.user$.subscribe(
+        (response) =>{
+          this.currentPlayer.email = response?.email;
+          console.log(this.currentPlayer.email);
+          this.getUserInfo(this.currentPlayer.email!);
+          console.log("does this fail");
+          this.getSongs();
+        }
+      );
+    }
   }
 
   getSongs()
@@ -33,22 +57,57 @@ export class JukeboxComponent implements OnInit,AfterViewInit {
       this.Songs = response;
       //randomize list of songs with Durstenfeld shuffle
       this.randomizeSongs();
-      
-      console.log(this.Songs);
-      
     }
     );
   }
 
-  ngAfterViewInit(): void {
-    //this.nextSong();
+  getUserInfo(p_email: string) {
+    this.profApi.getUserInfo(p_email).subscribe(
+      (response) => {
+        this.currentPlayer.id = response.id;
+        this.currentPlayer.gamesPlayed = response.gamesPlayed;
+        this.currentPlayer.currentScore = response.currentScore;
+      });
   }
+
+  updatePlayerProfile(profile: Profile) {
+    this.profApi.updatePlayerProfile(profile).subscribe(
+      (response) =>
+      { //after updating profile retrieve it
+        this.getUserInfo(this.currentPlayer.email!);
+      }
+    );
+  }
+
   randomizeSongs()
   {
+    
     for (let i = this.Songs.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [this.Songs[i], this.Songs[j]] = [this.Songs[j], this.Songs[i]];
     }
+    this.nextSong();
+  }
+  changeSongs()
+  {
+    this.getUserInfo(this.currentPlayer.email!);
+    if(this.currentPlayer.currentScore < Math.abs(this.changeSongsCost))
+    {
+      return;
+    }
+    this.currentPlayer.currentScore += this.changeSongsCost;
+    this.updatePlayerProfile(this.currentPlayer);
+    this.randomizeSongs();
+  }
+  skipSong()
+  {
+    this.getUserInfo(this.currentPlayer.email!);
+    if(this.currentPlayer.currentScore < Math.abs(this.skipSongCost))
+    {
+      return;
+    }
+    this.currentPlayer.currentScore += this.skipSongCost;
+    this.updatePlayerProfile(this.currentPlayer);
     this.nextSong();
   }
   nextSong() {
@@ -61,7 +120,7 @@ export class JukeboxComponent implements OnInit,AfterViewInit {
     this.song2 = this.Songs[1].songName;
     this.song3 = this.Songs[2].songName;
     audio.src = this.Songs[0].songURL;
-    audio.volume = 0.02;
+    audio.volume = this.audioVolume;
     audio.load();
     audio.play();
   }
@@ -72,5 +131,8 @@ export class JukeboxComponent implements OnInit,AfterViewInit {
     this.totalSongTime =audio.duration;
   }
 
-
+  onVolumeChange(e: any)
+  {
+    this.audioVolume = e.target.value /200;
+  }
 }
--- Front-End/app/src/app/components/login/login.component.ts
+++ Front-End/app/src/app/components/login/login.component.ts
@@ -16,7 +16,16 @@ export class LoginComponent implements OnInit {
   }
   login()
   {
-    this.auth.loginWithRedirect();
+    this.auth.loginWithRedirect(
+      {
+        /*
+        So I realized that if someone creates an account but doesn't go to the profile
+        before joining the game it wont create anything in the backend so this fixes that problem by forcing the 
+        person to go to the profile page after logging in.
+        */
+        appState: {target: '/profile'}
+      }
+    );
   }
   logout()
   {
